f7547f647ed3d5b5abc5af7b41ecf9e7240d3d37
Kikyou Akino <bellflower@web4u.jp>
2015-10-26 17:13:24 +0900

    tmux 2.1

--- tmux/build.sh
+++ tmux/build.sh
@@ -2,9 +2,12 @@ CURDIR=$(cd $(dirname "$0") && pwd)
 . "$CURDIR/../config.sh"
 
 NAME=tmux
+VERSION=2.1
+SRC_URL=https://github.com/tmux/tmux/releases/download/2.1/${NAME}-${VERSION}.tar.gz
 SRPM=https://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/source/SRPMS/t/tmux-2.0-2.fc23.src.rpm
 
 install_srpm
+get_src
 
 patch_spec
 
