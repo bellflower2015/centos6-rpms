4805a62e466e74d017a55eb494e002af1c5f7600
Kikyou Akino <bellflower@web4u.jp>
2015-12-24 11:45:29 +0900

    git 2.6.4

--- git/build.sh
+++ git/build.sh
@@ -2,9 +2,9 @@ CURDIR=$(cd $(dirname "$0") && pwd)
 . "$CURDIR/../config.sh"
 
 NAME=git
-VERSION=2.6.2
+VERSION=2.6.4
 SRC_URL=https://github.com/git/git/archive/v${VERSION}.tar.gz
-SRPM=https://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/source/SRPMS/g/git-2.6.2-1.fc24.src.rpm
+SRPM=https://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/source/SRPMS/g/git-2.6.4-1.fc24.src.rpm
 
 install_srpm
 
